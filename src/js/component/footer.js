033baa62fa45197c073dfb67bc36eb0042827bc1
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-09-01 17:02:57 -0400

    cambios

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			<a href="http://www.4geeksacademy.com">Diseñado por Rafael Nava</a>
+			<a href="https://github.com/rafaelnavam/Blog-de-Starwars_rafael-nava">Diseñado por Rafael Nava</a>
 		</p>
 	</footer>
 );
