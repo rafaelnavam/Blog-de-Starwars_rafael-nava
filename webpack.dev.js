88fe7174ab768e838af5af25ecf539d159e8ce18
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-09-01 17:09:06 -0400

    cambios

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,81 @@
-const path = require('path');
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+//Este archivo contiene configuraciones específicas para el entorno de desarrollo.
+//Incluye configuraciones para el servidor de desarrollo, el hot module replacement (HMR) y otras herramientas que facilitan el desarrollo.
 
+// Importa el módulo 'webpack' que se utiliza para empaquetar los archivos del proyecto.
+const webpack = require("webpack");
+
+// Importa el módulo 'path' que proporciona utilidades para trabajar con rutas de archivos y directorios.
+const path = require("path");
+
+// Importa la función 'merge' del módulo 'webpack-merge' que se usa para combinar configuraciones de webpack.
+const { merge } = require("webpack-merge");
+
+// Importa la configuración común desde 'webpack.common.js'.
+const common = require("./webpack.common.js");
+
+// Define el puerto en el que el servidor de desarrollo escuchará.
 const port = 3000;
+
+// Define la URL pública para WebSocket, inicialmente configurada para localhost en el puerto especificado.
 let publicUrl = `ws://localhost:${port}/ws`;
 
-//only for gitpod
-if(process.env.GITPOD_WORKSPACE_URL){
-  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+// Condición específica para entornos GitHub (Gitpod).
+if (process.env.GITPOD_WORKSPACE_URL) {
+  // Divide la URL del workspace en el esquema (http, https) y el host.
+  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split("://");
+  // Configura la URL pública para WebSocket usando 'wss' (WebSocket Secure) y el puerto adecuado para Gitpod.
   publicUrl = `wss://${port}-${host}/ws`;
 }
 
-//only for codespaces
-if(process.env.CODESPACE_NAME){
-  publicUrl = `wss://${process.env.CODESPACE_NAME}-${port}.preview.app.github.dev/ws`;
+// Condición específica para entornos GitHub Codespaces.
+if (process.env.CODESPACE_NAME) {
+  // Configura la URL pública para WebSocket usando 'wss' (WebSocket Secure) y el nombre del Codespace.
+  publicUrl = `wss://${process.env.CODESPACE_NAME}-${port}.app.github.dev/ws`;
 }
 
+// Exporta la configuración final de webpack combinada con la configuración común.
+// La función 'merge' combina el objeto común con las configuraciones específicas del entorno de desarrollo.
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'cheap-module-source-map',
-    devServer: {
-        port,
-        hot: true,
-        allowedHosts: "all",
-        historyApiFallback: true,
-        static: {
-          directory: path.resolve(__dirname, "dist"),
-        },
-        client: {
-          webSocketURL: publicUrl
-        },
+  // Establece el modo de webpack en 'development' para optimizaciones adecuadas para desarrollo.
+  mode: "development",
+
+  // Establece una herramienta de mapeo de origen barato para facilitar la depuración.
+  devtool: "cheap-module-source-map",
+
+  // Configuración del servidor de desarrollo.
+  devServer: {
+    // Especifica el puerto en el que el servidor de desarrollo escuchará.
+    port,
+
+    // Habilita la actualización de módulos en caliente, permitiendo recargar módulos en tiempo real sin recargar la página completa.
+    hot: true,
+
+    // Permite que el servidor acepte solicitudes desde cualquier host.
+    allowedHosts: "all",
+
+    // Permite la navegación HTML5 History API redirigiendo todas las solicitudes al índice HTML.
+    historyApiFallback: true,
+
+    // Configura el directorio estático desde donde el servidor servirá archivos.
+    static: {
+      directory: path.resolve(__dirname, "public"), // Sirve desde el directorio correcto
     },
-    plugins: []
+
+    // Configura el cliente del servidor para usar la URL de WebSocket especificada.
+    client: {
+      webSocketURL: publicUrl,
+    },
+    //   overlay: {
+    //     warnings: false,
+    //     errors: true,
+    //   },
+    //   logging: 'warn',
+    // },
+  },
+
+  // Lista de plugins que se usarán en el entorno de desarrollo.
+  plugins: [
+    // Plugin para permitir la actualización de módulos en caliente.
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 });
