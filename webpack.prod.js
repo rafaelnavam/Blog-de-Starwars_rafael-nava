8f82d270c33db4d8cd187e101b283c99727bfa5f
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-09-01 17:34:21 -0400

    Revert "commet/deploy"
    This reverts commit 923ef81f9beca237ae7cbdda51ff2c3fd4fc1466.
    
    Revert 'commet/deploy

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,32 +1,15 @@
-//Este archivo contiene configuraciones específicas para el entorno de producción.
-//Incluye configuraciones para optimización de archivos, minificación y otras mejoras de rendimiento.
-
-// Importa la función 'merge' del módulo 'webpack-merge' que se usa para combinar configuraciones de webpack.
-const { merge } = require("webpack-merge");
-
-// Importa la configuración común desde 'webpack.common.js'.
-const common = require("./webpack.common.js");
-
-// Importa el plugin 'dotenv-webpack' que carga variables de entorno desde un archivo .env.
-const Dotenv = require("dotenv-webpack");
-
-// Exporta la configuración final de webpack para el entorno de producción, combinada con la configuración común.
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
 module.exports = merge(common, {
-    // Establece el modo de webpack en 'production' para optimizaciones adecuadas para producción.
-    mode: "production",
-
-    // Configura la salida de los archivos empaquetados.
+    mode: 'production',
     output: {
-        // Define la ruta pública para referenciar los archivos en el servidor.
-        publicPath: "/",
+        publicPath: './'
     },
-
-    // Lista de plugins que se usarán en el entorno de producción.
     plugins: [
-        // Plugin para cargar variables de entorno desde un archivo .env.
         new Dotenv({
-            safe: true, // Verifica que todas las variables necesarias están definidas en .env.
-            systemvars: true, // Permite usar variables del sistema además de las definidas en el archivo .env.
-        }),
-    ],
+            safe: true,
+            systemvars: true
+        })
+    ]
 });
